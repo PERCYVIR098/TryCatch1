d580f408a308fde059c08e6bf12b59cb67457167
Whoo <120265907+PERCYVIR098@users.noreply.github.com>
2023-10-22 16:53:31 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Enter an integer: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            else
+            {
+                double squareRoot = Math.Sqrt(number);
+                Console.WriteLine($"Square root of {number} is: {squareRoot}");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number.");
+        }
+        finally
+        {
+            Console.WriteLine("Good Bye.");
+            
+        }
+    }
+}
